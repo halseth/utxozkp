e2e35e20794187946271ed9f4e80628d090bbb7f
Johan T. Halseth <johanth@gmail.com>
2024-10-22 14:12:35 +0200

    guest: use k256 to do tweak
    let us avoid using libsecp, and remove that dependency.

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -6,16 +6,60 @@ use rustreexo::accumulator::proof::Proof;
 use rustreexo::accumulator::stump::Stump;
 use sha2::{Digest, Sha512_256};
 
-use bitcoin::XOnlyPublicKey;
+use bitcoin::key::{UntweakedPublicKey};
+use bitcoin::{Amount, ScriptBuf, TapNodeHash, TapTweakHash, TxOut, WitnessVersion, XOnlyPublicKey};
+use bitcoin::script::{Builder, PushBytes};
 use bitcoin::consensus::encode::serialize;
-use bitcoin::secp256k1::Secp256k1;
-use bitcoin::{ScriptBuf, TxOut, Amount};
 use k256::schnorr;
 use k256::schnorr::signature::Verifier;
+use k256::elliptic_curve::sec1::ToEncodedPoint;
+
+pub fn new_p2tr(
+    internal_key: UntweakedPublicKey,
+    merkle_root: Option<TapNodeHash>,
+) -> ScriptBuf {
+    let output_key = tap_tweak(internal_key, merkle_root);
+    // output key is 32 bytes long, so it's safe to use `new_witness_program_unchecked` (Segwitv1)
+    new_witness_program_unchecked(WitnessVersion::V1, output_key.serialize())
+}
+
+fn new_witness_program_unchecked<T: AsRef<PushBytes>>(
+    version: WitnessVersion,
+    program: T,
+) -> ScriptBuf {
+    let program = program.as_ref();
+    debug_assert!(program.len() >= 2 && program.len() <= 40);
+    // In segwit v0, the program must be 20 or 32 bytes long.
+    debug_assert!(version != WitnessVersion::V0 || program.len() == 20 || program.len() == 32);
+    Builder::new().push_opcode(version.into()).push_slice(program).into_script()
+}
+
+
+fn tap_tweak(
+    internal_key: UntweakedPublicKey,
+    merkle_root: Option<TapNodeHash>,
+) -> XOnlyPublicKey {
+    let tweak = TapTweakHash::from_key_and_tweak(internal_key, merkle_root).to_scalar();
+
+    let pub_bytes = internal_key.serialize();
+    let pub_key : k256::PublicKey = schnorr::VerifyingKey::from_bytes(&pub_bytes).unwrap().into();
+    let pub_point = pub_key.to_projective();
+
+    let tweak_bytes = &tweak.to_be_bytes();
+    let tweak_point = k256::SecretKey::from_bytes(tweak_bytes.into()).unwrap().public_key().to_projective();
+
+    let tweaked_point = pub_point + tweak_point;
+    let compressed = tweaked_point.to_encoded_point(true);
+    let x_coordinate = compressed.x().unwrap();
+
+    let ver_key = schnorr::VerifyingKey::from_bytes(&x_coordinate).unwrap();
+
+    let pubx = XOnlyPublicKey::from_slice(ver_key.to_bytes().as_slice()).unwrap();
+
+    pubx
+}
 
 fn main() {
-    let secp = Secp256k1::verification_only();
-
     // read the input
     let msg_bytes: Vec<u8> = env::read();
     let priv_key: schnorr::SigningKey = env::read();
@@ -27,7 +71,7 @@ fn main() {
 
     // We'll check that the given public key corresponds to an output in the utxo set.
     let pubx = XOnlyPublicKey::from_slice(internal_key.to_bytes().as_slice()).unwrap();
-    let script_pubkey = ScriptBuf::new_p2tr(&secp, pubx, None);
+    let script_pubkey = new_p2tr(pubx, None);
     let utxo = TxOut {
         value: Amount::ZERO,
         script_pubkey,
